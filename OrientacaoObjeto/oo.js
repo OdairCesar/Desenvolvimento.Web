941f8f158ea1cfe598b681d7c9587aa9ad8e06d4
Odair Cesar Ferreira <odairferreira97@gmail.com>
2021-01-03 02:54:45 -0300

    Paradigma orientada a objetos no JS, formas diferentes de criar e estanciar um objeto. E formas diferentes de tratamento de atributos e função de cada objeto usando funções do Object

---
+++ OrientacaoObjeto/oo.js
@@ -0,0 +1,26 @@
+// NÃO EXECUTAVEL
+
+// Procedural
+processamento(valor1, valor2){
+    let manipularValor1 = valor1;
+
+    console.log(manipularValor1);
+}// Foco era a função
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    processamento(){
+        console.log(valor1)
+    }
+}
+objeto.processamento()// Foco é o objeto
+
+/** 
+ * Principais Principios
+ * - Polimorfismo
+ * - Encapsulamento
+ * - Abstração
+ * - Herança
+ */
